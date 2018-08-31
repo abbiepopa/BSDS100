ffcf1313ec0511ac38eda70b0106745909551a30
Abbie Popa <apopa@usfca.edu>
2018-08-31 10:33:30 -0700

    add class code from aug 30

---
+++ class_code/lecture4_180830.R
@@ -0,0 +1,27 @@
+x <- 7
+is.atomic(x)
+y <- "Abbie"
+is.atomic(y)
+typeof(x)
+typeof(y)
+
+z<- 1:10
+z
+is.atomic(z)
+typeof(z)
+
+length(x)
+length(z)
+
+
+j <- c(1, 2, 6)
+typeof(j)
+
+int_vec <- c(1L, 2L, 6L)
+
+int_vec
+typeof(int_vec)
+
+logical_vec <- c(TRUE, FALSE, T, F)
+
+logical_vec
